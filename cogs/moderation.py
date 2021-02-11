a0c8b8d2fb7623517c6767f9a108307c8014b586
Reviath <28-Reviath@users.noreply.git.randomchars.net>
2021-02-11 17:03:37 -0500

    Added ban command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -9,6 +9,21 @@ class Moderation(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print('Moderation commands are loaded!')
+
+    @commands.command(brief="Ban the user", description="Allow's you to ban the user")
+    @commands.has_permissions(ban_members = True)
+    async def ban(self, ctx, member:discord.User = None, reason = None):
+        if member == None:
+            await ctx.send('You need to mention someone.')
+            return
+        if member == ctx.message.author:
+            await ctx.send('You can\'t ban yourself.')
+            return
+        if reason == None:
+            reason = "Unspecified"
+        message = f"You have been banned from {ctx.guild.name} for {reason}."
+        await ctx.guild.ban(member, reason=reason)
+        await ctx.send(f"{member} is banned!")
         
 def setup(client):
     client.add_cog(Moderation(client))
