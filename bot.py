64f4cf84e38f3546f694ad5fe744d565daf220d5
Reviath <50050275+Reviath01@users.noreply.github.com>
2021-02-14 02:11:56 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,12 @@ client = commands.Bot(command_prefix = get_prefix)
 status = cycle(['Prefix: !', 'Reviath'])
 
 @client.event
+async def on_command_error(ctx, error):
+    log = client.get_channel(790640302452375562)
+    await ctx.send(error)
+    await log.send(f'Error on server `{ctx.guild.name}` \n{error}')
+
+@client.event
 async def on_guild_join(guild):
     with open('prefixes.json', 'r') as f:
         prefixes = json.load(f)
@@ -36,14 +42,21 @@ async def on_guild_remove(guild):
 
 @client.event
 async def on_ready():
+    log = client.get_channel(790640302452375562)
     change_status.start()
     print(client.user.display_name + '#' + client.user.discriminator + ' is ready!')
+    await log.send('I am ready to use!')
 
 @tasks.loop(seconds=15)
 async def change_status():
     await client.change_presence(activity=discord.Game(next(status)))
 
-@client.command(brief="Author command", description="Author command")
+@client.command(brief="Shows my author", description="Shows my author")
+async def author(ctx):
+    authorembed = discord.Embed(description="My Author: \n<@!770218429096656917> ([Reviath#0001](https://discord.com/users/770218429096656917))", colour=discord.Colour.purple())
+    await ctx.send(embed = authorembed)
+
+@client.command(brief="Author command", description="Author command", hidden = True)
 async def load(ctx, extension):
     if str(ctx.author.id) == "770218429096656917":
         client.load_extension(f'cogs.{extension}')
@@ -51,7 +64,7 @@ async def load(ctx, extension):
     else:
         await ctx.send('This command is only for my author.')
 
-@client.command(brief="Author command", description="Author command")
+@client.command(brief="Author command", description="Author command", hidden = True)
 async def unload(ctx, extension):
     if str(ctx.author.id) == "770218429096656917":
         client.unload_extension(f'cogs.{extension}')
@@ -59,7 +72,7 @@ async def unload(ctx, extension):
     else:
         await ctx.send('This command is only for my author.')
 
-@client.command(brief="Author command", description="Author command")
+@client.command(brief="Author command", description="Author command", hidden = True)
 async def reload(ctx, extension):
     if str(ctx.author.id) == "770218429096656917":
         client.unload_extension(f'cogs.{extension}')
@@ -68,7 +81,7 @@ async def reload(ctx, extension):
     else:
         await ctx.send('This command is only for my author')
 
-@client.command(brief="Author command", description="Author command")
+@client.command(brief="Author command", description="Author command", hidden = True)
 async def shutdown(ctx):
     if str(ctx.author.id) == "770218429096656917":
         await ctx.send('Shuting down!')
@@ -80,11 +93,12 @@ def restart_program():
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
-@client.command(brief="Author command", description="Author command")
+@client.command(brief="Author command", description="Author command", hidden = True)
 async def restart(ctx):
     if str(ctx.author.id) == "770218429096656917":
         await ctx.send("Restarting...")
-        print('Restarting...')
+        log = client.get_channel(790640302452375562)
+        await log.send('Restarting...')
         restart_program()
     else:
         await ctx.send('This command is only for my author.')
